4fe85c2930628fa89cd5c42e449e0225b1bc5366
Nikita Rodionov <texhik620953@gmail.com>
2024-01-11 00:01:35 +0300

    Added custom types

--- src/lib/generateTypes/go.ts
+++ src/lib/generateTypes/go.ts
@@ -5,6 +5,12 @@ export default async function generatePyTypes(api) {
   let ret = "";
   const types = [];
   
+  ret += `import (
+    "github.com/google/uuid"
+    "time"
+  )
+  `
+
   ret += Object.values(collections).map((collection) => {
       const collectionName = collection.collection;
       const typeName = collectionName
@@ -17,21 +23,25 @@ ${Object.values(collection.fields).map((x) => `  ${x.field}: ${getType(x.type)}`
 .join("\n}\n\n");
   ret += "\n\n";
 
-  ret += `class CustomDirectusTypes(TypedDict):
-${types.map((x) => `  ${x}`).join("\n")}
-`;
-
-  ret += "\n";
-
   return ret;
 }
 
 function getType(directusType: string) {
+  
+  if (["uuid"].includes(directusType)) return "uuid.UUID";
+  if (["timestamp"].includes(directusType)) return "time.Time";
+  if (["json", "csv"].includes(directusType)) return "interface{}";
+  
   if (["integer"].includes(directusType)) return "int";
   if (["bigInteger"].includes(directusType)) return "int64";
 
   if (["float"].includes(directusType)) return "float";
   if (["decimal"].includes(directusType)) return "float64";
   if (["boolean"].includes(directusType)) return "bool";
+  if (["text", "string"].includes(directusType)) return "string";
+
+  if (["alias"].includes(directusType)) return "interface{}";
+  if (["hash"].includes(directusType)) return "string";
+  
   return "interface{}";
 }
